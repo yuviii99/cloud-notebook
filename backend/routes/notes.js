68283c25a1a8911967992e1f3667bd413eb9eb02
Yuvraj Singh Rana <ranayuvraj99@gmail.com>
2024-04-29 00:23:23 +1000

    Added updatenote and deletenode endpoints

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -49,4 +49,54 @@ router.post(
   }
 );
 
+// ROUTE 3 : Update an existing note using PUT : /api/notes/updatenote. Login required
+
+router.put('/updatenote/:id', fetchuser, async (req, res) => {
+    const {title, description, tag} = req.body;
+
+    // Create new note object
+    const newNote = {};
+    if(title){
+        newNote.title = title
+    };
+    if(description){
+        newNote.description = description
+    };
+    if(tag){
+        newNote.tag = tag
+    };
+
+    // Find the note to be updated
+    let note = await Note.findById(req.params.id);
+    if(!note){
+        return res.status(404).send("Not found");
+    }
+
+    if(note.user.toString() !== req.user.id){
+        return res.status(401).send("Not allowed!");
+    }
+
+    note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new: true});
+
+    res.json(note);
+
+});
+
+// ROUTE 4 : Delete a note using DELETE : /api/notes/deletenote. Login required
+
+router.delete('/deletenote/:id', fetchuser, async(req, res) => {
+    let note = await Note.findById(req.params.id);
+    if(!note){
+        return res.status(404).send("Not found");
+    }
+
+    if(note.user.toString() !== req.user.id){
+        return res.status(401).send("Not allowed!");
+    }
+
+    note = await Note.findByIdAndDelete(req.params.id);
+
+    res.status(200).send("Successfully deleted!");
+});
+
 module.exports = router;
